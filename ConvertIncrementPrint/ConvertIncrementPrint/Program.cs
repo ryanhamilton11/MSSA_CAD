41d7dcbd617a51a5382dda98f70c5d06edf349f3
ryanhamilton11 <70560407+ryanhamilton11@users.noreply.github.com>
2021-04-07 21:52:42 -0600

    Added ConvertIncrementPrint Project

---
+++ ConvertIncrementPrint/ConvertIncrementPrint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConvertIncrementPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "17";
+            int count = System.Int32.Parse(input);
+            ++count;
+            Console.WriteLine($"{count}");
+        }
+    }
+}
