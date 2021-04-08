0e701345aa59c060372b9d4024dc9f38063241fd
ryanhamilton11 <70560407+ryanhamilton11@users.noreply.github.com>
2021-04-07 21:53:14 -0600

    Added Fibonacci Project

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentFib = 0;
+            int nextFib = 1;
+
+            for (int i = 0; i < 20; i++)
+            {
+                int nextNextFib = currentFib + nextFib;
+                Console.WriteLine($"{currentFib} ");
+                currentFib = nextFib;
+                nextFib = nextNextFib;
+            }
+        }
+    }
+}
