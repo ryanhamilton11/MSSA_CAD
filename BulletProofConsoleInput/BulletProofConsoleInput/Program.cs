6d8cf4ba8fd7df74637b393103467b3aa30bb386
ryanhamilton11 <70560407+ryanhamilton11@users.noreply.github.com>
2021-04-07 21:50:45 -0600

    Added BulletProofConsoleInput Project

---
+++ BulletProofConsoleInput/BulletProofConsoleInput/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BulletProofConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = IntCheck();
+            double y = DoubleCheck();
+        }
+        static int IntCheck()
+        {
+            int y = 0;
+            bool succeedInt = false;
+
+            while (succeedInt == false)
+            {
+                try
+                {
+                    Console.Write("Please enter a integer: ");
+                    y = int.Parse(Console.ReadLine());
+                    succeedInt = true;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return y;
+        }
+
+        static double DoubleCheck()
+        {
+            double x = 0;
+            bool succeedDouble = false;
+
+            while (succeedDouble == false)
+            {
+                try
+                {
+                    Console.Write("Please enter a double: ");
+                    x = double.Parse(Console.ReadLine());
+                    succeedDouble = true;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return x;
+        }
+    }
+}
