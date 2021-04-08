09f3f70821368f60d61992739dae2907604c075b
ryanhamilton11 <70560407+ryanhamilton11@users.noreply.github.com>
2021-04-07 22:02:56 -0600

    Added PartyInvites Project

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //FIELDS
+
+
+        //CONSTRUCTORS
+        
+        
+        //METHODS
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse  //POCO - Plain, Old, C#, Object
+    {
+        //FIELDS
+        private string name;     //Unlimited values can be stored
+        private string email;    //Unlimited values can be stored
+        private string phone;    //Unlimited values can be stored
+        private bool? willAttend; //True, False, Null
+
+        //PROPERTIES
+
+        //CONSTRUCTORS
+
+
+        //METHODS
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            if(name != null &&)
+        }
+    }
+}
