fa813ca8f7e6d9d49ade56f0303b3201cc5c9920
ryanhamilton11 <70560407+ryanhamilton11@users.noreply.github.com>
2021-04-07 21:50:00 -0600

    Added ASPHelloWorld Project

---
+++ ASPHelloWorld/ASPHelloWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPHelloWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
