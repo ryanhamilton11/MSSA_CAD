0f51f11526f8297fbebb24748fd175c0084db869
ryanhamilton11 <70560407+ryanhamilton11@users.noreply.github.com>
2021-04-07 22:07:32 -0600

    Added Welcome Project

---
+++ Welcome/Welcome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ryan Welcom to C#!");
+        }
+    }
+}
