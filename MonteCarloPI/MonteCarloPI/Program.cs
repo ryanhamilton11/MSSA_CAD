f79ed6e89857142c5fa0e734f5a77bf2928f66e0
ryanhamilton11 <70560407+ryanhamilton11@users.noreply.github.com>
2021-04-07 21:57:33 -0600

    Added MonteCarloPI Project

---
+++ MonteCarloPI/MonteCarloPI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MonteCarloPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Random rnd = new System.Random();
+            int iterations;
+            iterations = int.Parse(args[0]);
+
+            int countinside = 0;
+            int countoutside = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                double x;
+                double y;
+                (x, y) = RandomXY(rnd);
+
+                double c = Hypotenuse(x, y);
+                Console.WriteLine(c);
+                if (c <= 1.0)
+                {
+                    countinside++;
+                }
+                else
+                {
+                    countoutside++;
+                }
+            }
+
+            Console.WriteLine($"Values under 1: {countinside}");
+            Console.WriteLine($"Values over 1: {countoutside}");
+
+            double myPi = 4.0 * countinside / iterations;
+            Console.WriteLine($"My estimate of PI is: {myPi}");
+            myPi -= Math.PI;
+            Console.WriteLine($"The difference between my estimate of PI and the actual value of PI is: {myPi}");
+        }
+
+        static (double x, double y) RandomXY(System.Random rnd)
+        {
+            double x = rnd.NextDouble();
+            Console.WriteLine(x);
+            double y = rnd.NextDouble();
+            Console.WriteLine(y);
+            return (x, y);
+        }
+
+        static double Hypotenuse(double x, double y)
+        {
+            double distance = Math.Sqrt(x * x + y * y);
+            return distance;
+        }
+    }
+}
