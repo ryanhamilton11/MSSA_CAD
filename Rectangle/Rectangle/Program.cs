f47d1f78d0935f0fd3352000172035772e9a308b
ryanhamilton11 <70560407+ryanhamilton11@users.noreply.github.com>
2021-04-07 22:03:35 -0600

    Added Rectangle Project

---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(-1, -2);
+            r1.SetLength(2.0);
+            r1.SetWidth(-0.1);
+
+            Rectangle r2 = new Rectangle();
+            r2.SetLength(3);
+            r2.SetLength(-2);
+
+            Rectangle r3 = new Rectangle();
+            r3.SetLength(0);
+            r3.SetWidth(0);
+        }
+    }
+}
